6bac369c65cb9e1fecd6d78c2122d6fa0dc7349e
Jun <hyeonjun_park@Jun-MacBook-Pro.local>
2021-04-21 10:38:22 +0900

    WorkSmall 최초 commit

---
+++ src/main/java/com/atm/worksmall/WorksmallApplication.java
@@ -0,0 +1,13 @@
+package com.atm.worksmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorksmallApplication.class, args);
+    }
+
+}
